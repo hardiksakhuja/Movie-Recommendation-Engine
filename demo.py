3bbbd82334b570a28c4a7afad9f507314b8c0778
Hardik <hardiksakhuja421997@gmail.com>
2019-03-27 21:22:40 +0530

    working demographic

--- demo.py
+++ demo.py
@@ -4,9 +4,27 @@ import numpy as np
 df1 = pd.read_csv('tmdb_5000_credits.csv')
 df2 = pd.read_csv('tmdb_5000_movies.csv')
 
-df1.columns = ['id','title','cast','crew']
-df2 = df2.merge(df1,on='id')
+df1.columns = ['id','Title','cast','crew']
+#df2 = df2.merge(df1,on='id')
+df2 = pd.merge(df1,df2,on='id')
 print(df2.head(5))
 
-C=df2['vote_average'].mean()
-print(C)
+c=df2['vote_average'].mean()
+#print(C)
+m = df2['vote_count'].quantile(0.9)
+print(m)
+#qual_movies = df2.copy().loc[df2['vote_count'] >= m]
+qual_movies = df2[df2['vote_count'] >= m]
+print(qual_movies.shape)
+
+def value(x,m=m,c=c):
+    v=x['vote_count']
+    r=x['vote_average']
+
+    return (v/(v+m) * r) + (m/(m+v) * c)
+
+qual_movies['score'] = qual_movies.apply(value,axis=1)
+q_movies = qual_movies.sort_values('score',ascending=False)
+print(q_movies.head(2))
+
+print(q_movies[['Title','vote_count','vote_average','score']].head(10))
